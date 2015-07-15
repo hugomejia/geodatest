9ed661ddfd185695c988966790e3d1fdf7596a33
AlandEr2612 <alberto93-14@hotmail.com>
2015-07-14 22:42:24 -0500

    Fixed issue of GC.

--- app/src/main/java/com/arrg/android/app/geoda/SplashScreenActivity.java
+++ app/src/main/java/com/arrg/android/app/geoda/SplashScreenActivity.java
@@ -19,7 +19,6 @@ import android.widget.TextView;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
